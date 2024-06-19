3c42827adc43ac47f1a326d8bcd4e2b95b6e79e1
Andrei <24367273+hurxan@users.noreply.github.com>
2024-06-19 11:46:07 +0200

    test

--- info.php
+++ info.php
@@ -72,6 +72,7 @@
         var ISI = parseInt(<?php echo $row["ISI"]; ?>);
         var nAFC = parseInt(<?php echo $row["nAFC"]; ?>);
         var feedback = <?php echo $row["feedback"]; ?>;
+	console.log(freq);
     </script>
     <script type="text/javascript"
             src="js/generatorSoundAndNoise.js<?php if (isset($_SESSION['version'])) echo "?{$_SESSION['version']}"; ?>"
